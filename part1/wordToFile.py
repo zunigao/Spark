80fad63c413caee87008f39ef3d19ac745489239
Orlando Zuniga <orlandoismaelzuniga@gmail.com>
2018-11-06 21:07:00 -0600

    first step

--- part1/wordToFile.py
+++ part1/wordToFile.py
@@ -1,1 +1,13 @@
 #Write using the wordcount2 example.
+#Questions?
+
+import pyspark.sql.functions as psf
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("wordcount").getOrCreate()
+
+lines = spark.read.text("input.txt")
+
+words1 = lines.select(psf.input_file_name(), \
+                      psf.split("value","\s")) \
+              .toDF("part1out.txt", "wordlist")
