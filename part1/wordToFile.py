49f3c5b6ae6f6b22c718f7e466d198d0674d4f10
Blake Johnson <blakeconnorjohnson@gmail.com>
2018-11-08 21:36:14 -0600

    part 1 final done

--- part1/wordToFile.py
+++ part1/wordToFile.py
@@ -7,37 +7,42 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("wordcount").getOrCreate()
 
-lines = spark.read.text("*.txt")
+lines = spark.read.text("/Accounts/courses/cs348/gutenberg/*.txt")
 
-words1 = lines.select(psf.input_file_name(), \
+data = lines.select(psf.input_file_name(), \
                       psf.split("value","\s")) \
               		.toDF("filename", "wordlist")
 
-words1a = words1.select(psf.split("filename","/"), "wordlist") \
+data = data.select(psf.split("filename","/"), "wordlist") \
 			.toDF("filenamesplit", "wordlist")
 
 
-words1b = words1a.select("wordlist",\
+data = data.select("wordlist",\
 				words1a.filenamesplit[ \
             	psf.size(words1a.filenamesplit)-1])\
            .toDF( "wordlist", "realfilename")
 
-words2 = words1b.select(psf.explode("wordlist"), "realfilename") \
+data = data.select(psf.explode("wordlist"), "realfilename") \
               .toDF("word", "realfilename")
 
 
 
 
-words2 = words2.dropDuplicates()
+data = data.dropDuplicates() # This is necessary for multiples in same file
 
-#.agg(psf.count("word")) \
 
-results = words2.groupBy("word") \
+
+data = data.groupBy("word") \
                 .agg(psf.collect_set("realfilename")) \
                 .toDF("word", "the count")
                 
-df = results.select("word", "the count").toDF("word", "filename")
+df = data.select("word", "the count").toDF("word", "filename")
 
+
+'''Our program wasn't working due to the whole
+	"csv's don't do lists" thing. we solved this by turning the list
+	into a string, with some code we found from stackoverflow. it's simple
+	code, and we get what it does.'''
 from pyspark.sql.functions import udf
 from pyspark.sql.types import StringType
 
@@ -45,6 +50,10 @@ def array_to_string(my_list):
     return '[' + ','.join([str(elem) for elem in my_list]) + ']'
 
 array_to_string_udf = udf(array_to_string,StringType())
+
+# Ok, back to code we actually wrote 
+
+
 
 df = df.withColumn('filenames',array_to_string_udf(df["filename"]))
 
